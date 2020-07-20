dc266878d33dd14862069ae553f625b4228443f8
Szymon Rucinski <s.rucinski@AMDC2073.digital.local>
2020-07-20 11:23:51 +0200

    working on file parsing

---
+++ findMatchColor.js
@@ -0,0 +1,33 @@
+
+var colourNameToHex = require('./colourNameToHex.js');
+const fetch = require('node-fetch');
+const { error } = require('console');
+
+module.exports.findMatchColor = function findMatchColor(colorName) {
+
+  var hexColor = colourNameToHex(colorName);
+  hexColor = hexColor.slice(1,hexColor.length);
+
+  console.log(hexColor);
+
+
+  // var response = http.getUrl(config.get('http://thecolorapi.com/scheme?') + '/hex=' + hexColor + '&mode=analogic-complement&count=3', {format: 'json'});
+  
+  // fetch('http://thecolorapi.com/scheme?'+'/hex=' + hexColor + '&mode=analogic-complement&count=3')
+
+
+
+  fetch('https://www.thecolorapi.com/scheme?hex=008000&mode=monochrome&count=5&format=json')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+  })
+  .catch(error);
+
+  return hexColor;
+
+    
+  
+    
+    
+  };
